516cceb116e7ccf557f05f762c2ce5f74a0e7f55
cesar <cesar.marsfall@gmail.com>
2025-11-13 22:27:16 -0800

    modified 1.0

--- guide/memory/stack.c
+++ guide/memory/stack.c
@@ -3,25 +3,43 @@
 #include <stdint.h>
 
 void c() {
+	/* variable (z) type integer (int8_t) is a local variable stored in c()'s stack frame */
 	int8_t z = 55;
 	printf("%p\n", &z);
+
+    /* When c() returns, c()'s stack frame will be popped (freed) */
 }
 
 void b() {
+	/* variable (y) type integer (int8_t) is a local variabel stored in b()'s stack frame */
 	int8_t y = 28;
 	printf("%p\n", &y);
+
+    /* calling c() pushes a new stack frame on top of b() */
 	c();
+
+    /* when b() returns, b()'s stack frame will be popped (freed) */
 }
 
 void a() {
+	/* variable (x) type integer (int8_t) is a local variable stored in a()'s stack frame */
 	int8_t x = 10;
 	printf("%p\n", &x);
+
+    /* calling b() pushes a new stack frame on top of a(), allocates memory for that function */
 	b();
+
+    /* when a() returns, a()'s stack frame will be popped (freed) */
 }
 
 int main() {
+	/* variable (value) type integer (int8_t) is a local variable stored in main()'s stack frame */
 	int8_t value = 2;
 	printf("%p\n", &value);
+
+	/* calling a() pushes a()'s stack frame on top of main(), allocates memory for that function */
 	a();	
+
+	/* when main() returns, the program ends and its frame is freed, aka deallocation */
 	return 0;
 }
