4ee901aba5b67776a10f67039d08b6c2e4d4889a
cesar martinez <cesar.marsfall@gmail.com>
2026-02-15 23:59:26 -0800

    new files

--- sandbox.c
+++ sandbox.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include <stdint.h>
 /*
 	general use, for creative thinking (sandbox)
 */
 
 int main(int argc, char *argv[]) {
-
+	printf("%d bytes\n", sizeof(uint32_t));
 	return 0;	
 
 }
