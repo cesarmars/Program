7a6069a27c98e5bbfd266fd2f22d824084387b52
cesar martinez <cedar@gmail.com>
2026-07-28 18:01:01 -0700

    ssh update

--- ref/sandbox.c
+++ ref/sandbox.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
 /*
 	general use, for creative thinking (sandbox)
 */
 
-int main(int argc, char *argv[]) {
-	int arr[]={1,2,4};
-
-	int s = 0;
-	for(int i=0; i < sizeof(arr)/sizeof(int); i++) s=s+*(arr+i);
-
-	printf("%d\n", s);
-		
+int main() {
+	int8_t x = 0x7A;
+	printf("%d\n", x);			
 	return 0;	
 
 }
